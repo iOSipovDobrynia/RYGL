8efc549ed6c6a522a74d0d65a80c058a9c89b082
iOSipovDobrynia <dobrynia.osipov@gmail.com>
2023-07-03 21:45:37 +0300

    commit

--- RYGL/ViewController.swift
+++ RYGL/ViewController.swift
@@ -9,11 +9,56 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var redLightView: UIView!
+    @IBOutlet var yellowLightView: UIView!
+    @IBOutlet var greenLightView: UIView!
+    
+    @IBOutlet var changeLightButton: UIButton!
+    
+    private var countOfTap = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        lightIsOff(for: redLightView)
+        lightIsOff(for: yellowLightView)
+        lightIsOff(for: greenLightView)
+    }
+    
+    override func viewDidLayoutSubviews() {
+        makeViewCircle(for: redLightView)
+        makeViewCircle(for: yellowLightView)
+        makeViewCircle(for: greenLightView)
     }
 
-
+    @IBAction func changeLightButtonDidPressed() {
+        countOfTap += 1
+        
+        if countOfTap == 1 {
+            changeLightButton.setTitle("Next", for: .normal)
+        }
+        
+        switch countOfTap % 3 {
+        case 1:
+            lightIsOn(for: redLightView)
+            lightIsOff(for: greenLightView)
+        case 2:
+            lightIsOn(for: yellowLightView)
+            lightIsOff(for: redLightView)
+        default:
+            lightIsOn(for: greenLightView)
+            lightIsOff(for: yellowLightView)
+        }
+    }
+    
+    private func lightIsOff(for light: UIView) {
+        light.alpha = 0.3
+    }
+    private func lightIsOn(for light: UIView) {
+        light.alpha = 1
+    }
+    private func makeViewCircle(for view: UIView) {
+        view.layer.cornerRadius = view.frame.width / 2
+    }
 }
 
